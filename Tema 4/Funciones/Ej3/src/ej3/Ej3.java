027ce3878505e192e35dac39098f31636afac544
secrettalex <meleroalejandro2007@gmail.com>
2025-10-24 15:35:41 +0200

    Organizar
    Tema 2 y 3

---
+++ Tema 4/Funciones/Ej3/src/ej3/Ej3.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ej3;
+
+/**
+ *
+ * @author meler
+ */
+public class Ej3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /* Realizar un programa que mediante un método que no devuelve ningún valor, 
+        llamado cajaTexto recibe un String y lo muestra rodeado con un borde. Probarlo desde main.
+String: hola amigos
+La salida será:
+**************
+* hola amigos *
+**************  */
+
+        public static void (String cajaTexto)
+    }
+
+}
