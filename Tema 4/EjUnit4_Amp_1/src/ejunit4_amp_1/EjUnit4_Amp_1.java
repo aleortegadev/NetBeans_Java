9e4a32bfe3937b8d8d956ec47cb25039e16c6fb3
secrettalex <meleroalejandro2007@gmail.com>
2025-10-28 13:54:53 +0100

    Tema 5 (Arrays) 1
    Comienzo Tema 5 Arrays

---
+++ Tema 4/EjUnit4_Amp_1/src/ejunit4_amp_1/EjUnit4_Amp_1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejunit4_amp_1;
+
+/**
+ *
+ * @author Ale
+ */
+public class EjUnit4_Amp_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        public static void  menu () {
+            bollean repetir = true;1
+        }
+    }
+    
+}
